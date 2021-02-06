412b0f138b5ac5efa8f382727550eeb638c8c905
Полхович Дмитрий <polhovich.dm@mail.ru>
2021-02-06 20:13:58 +0300

    Add tests

---
+++ UnitTests/HubChaining.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Bson;
+using NiL.Hub;
+
+namespace Tests.Hubs
+{
+    [TestClass]
+    public sealed class HubChaining
+    {
+        [TestInitialize] public void Init() => AppDomain.CurrentDomain.FirstChanceException += currentDomain_FirstChanceException;
+        [TestCleanup] public void Cleanup() => AppDomain.CurrentDomain.FirstChanceException -= currentDomain_FirstChanceException;
+        private void currentDomain_FirstChanceException(object sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e) { }
+
+        [TestMethod]
+        public void SimpleChain()
+        {
+            using var hub0 = new Hub("hub 0");
+            var hub0EndPoint = new IPEndPoint(IPAddress.Loopback, 4500);
+            hub0.StartListening(hub0EndPoint);
+
+            using var hub1 = new Hub("hub 1");
+            var hub1EndPoint = new IPEndPoint(IPAddress.Loopback, 4501);
+            hub1.StartListening(hub1EndPoint);
+
+            using var hub2 = new Hub("hub 2");
+
+            Task.WaitAll(hub1.Connect(hub0EndPoint),
+                         hub2.Connect(hub1EndPoint));
+
+            var hub0KnownHubs = hub0.KnownHubs.ToArray();
+            Assert.AreEqual(2, hub0KnownHubs.Length);
+            Assert.IsTrue(hub0KnownHubs.Any(x => x.Name == "hub 1"));
+            Assert.IsTrue(hub0KnownHubs.Any(x => x.Name == "hub 2"));
+
+            var hub1KnownHubs = hub1.KnownHubs.ToArray();
+            Assert.AreEqual(2, hub1KnownHubs.Length);
+            Assert.IsTrue(hub1KnownHubs.Any(x => x.Name == "hub 0"));
+            Assert.IsTrue(hub1KnownHubs.Any(x => x.Name == "hub 2"));
+
+            var hub2KnownHubs = hub2.KnownHubs.ToArray();
+            Assert.AreEqual(2, hub1KnownHubs.Length);
+            Assert.IsTrue(hub2KnownHubs.Any(x => x.Name == "hub 0"));
+            Assert.IsTrue(hub2KnownHubs.Any(x => x.Name == "hub 1"));
+        }
+
+        [TestMethod]
+        public void ConnectInTheMiddleOfChain()
+        {
+            using var hub0 = new Hub("hub 0");
+            var hub0EndPoint = new IPEndPoint(IPAddress.Loopback, 4500);
+            hub0.StartListening(hub0EndPoint);
+
+            using var hub1 = new Hub("hub 1");
+            var hub1EndPoint = new IPEndPoint(IPAddress.Loopback, 4501);
+            hub1.StartListening(hub1EndPoint);
+
+            using var hub2 = new Hub("hub 2");
+
+            Task.WaitAll(hub1.Connect(hub0EndPoint),
+                         hub2.Connect(hub1EndPoint));
+
+            using var hub3 = new Hub("hub 3");
+            hub3.Connect(hub1EndPoint).Wait();
+
+            var hub0KnownHubs = hub0.KnownHubs.ToArray();
+            Assert.AreEqual(3, hub0KnownHubs.Length);
+            Assert.IsTrue(hub0KnownHubs.Any(x => x.Name == "hub 1"));
+            Assert.IsTrue(hub0KnownHubs.Any(x => x.Name == "hub 2"));
+            Assert.IsTrue(hub0KnownHubs.Any(x => x.Name == "hub 3"));
+
+            var hub1KnownHubs = hub1.KnownHubs.ToArray();
+            Assert.AreEqual(3, hub1KnownHubs.Length);
+            Assert.IsTrue(hub1KnownHubs.Any(x => x.Name == "hub 0"));
+            Assert.IsTrue(hub1KnownHubs.Any(x => x.Name == "hub 2"));
+            Assert.IsTrue(hub1KnownHubs.Any(x => x.Name == "hub 3"));
+
+            var hub2KnownHubs = hub2.KnownHubs.ToArray();
+            Assert.AreEqual(3, hub2KnownHubs.Length);
+            Assert.IsTrue(hub2KnownHubs.Any(x => x.Name == "hub 0"));
+            Assert.IsTrue(hub2KnownHubs.Any(x => x.Name == "hub 1"));
+            Assert.IsTrue(hub2KnownHubs.Any(x => x.Name == "hub 3"));
+
+            var hub3KnownHubs = hub3.KnownHubs.ToArray();
+            Assert.AreEqual(3, hub3KnownHubs.Length);
+            Assert.IsTrue(hub3KnownHubs.Any(x => x.Name == "hub 0"));
+            Assert.IsTrue(hub3KnownHubs.Any(x => x.Name == "hub 1"));
+            Assert.IsTrue(hub3KnownHubs.Any(x => x.Name == "hub 2"));
+        }
+
+        [TestMethod]
+        public void DisnnectFromTheMiddleOfChain()
+        {
+            using var hub0 = new Hub("hub 0");
+            var hub0EndPoint = new IPEndPoint(IPAddress.Loopback, 4500);
+            hub0.StartListening(hub0EndPoint);
+
+            using var hub1 = new Hub("hub 1");
+            var hub1EndPoint = new IPEndPoint(IPAddress.Loopback, 4501);
+            hub1.StartListening(hub1EndPoint);
+
+            using var hub2 = new Hub("hub 2");
+
+            Task.WaitAll(hub1.Connect(hub0EndPoint),
+                         hub2.Connect(hub1EndPoint));
+
+            using var hub3 = new Hub("hub 3");
+            hub3.Connect(hub1EndPoint).Wait();
+            hub3.Connections.First().Disconnect();
+
+            Thread.Sleep(10);
+
+            var hub0KnownHubs = hub0.KnownHubs.ToArray();
+            Assert.AreEqual(2, hub0KnownHubs.Length);
+            Assert.IsTrue(hub0KnownHubs.Any(x => x.Name == "hub 1"));
+            Assert.IsTrue(hub0KnownHubs.Any(x => x.Name == "hub 2"));
+
+            var hub1KnownHubs = hub1.KnownHubs.ToArray();
+            Assert.AreEqual(2, hub1KnownHubs.Length);
+            Assert.IsTrue(hub1KnownHubs.Any(x => x.Name == "hub 0"));
+            Assert.IsTrue(hub1KnownHubs.Any(x => x.Name == "hub 2"));
+
+            var hub2KnownHubs = hub2.KnownHubs.ToArray();
+            Assert.AreEqual(2, hub2KnownHubs.Length);
+            Assert.IsTrue(hub2KnownHubs.Any(x => x.Name == "hub 0"));
+            Assert.IsTrue(hub2KnownHubs.Any(x => x.Name == "hub 1"));
+
+            var hub3KnownHubs = hub3.KnownHubs.ToArray();
+            Assert.AreEqual(0, hub3KnownHubs.Length);
+        }
+
+        [TestMethod]
+        public void TwoConnections()
+        {
+            using var hub0 = new Hub("hub 0");
+            var hub0EndPoint = new IPEndPoint(IPAddress.Loopback, 4500);
+            hub0.StartListening(hub0EndPoint);
+
+            using var hub1 = new Hub("hub 1");
+
+            Task.WaitAll(hub1.Connect(hub0EndPoint),
+                         hub1.Connect(hub0EndPoint));
+
+            var hub0KnownHubs = hub0.KnownHubs.ToArray();
+            Assert.AreEqual(1, hub0KnownHubs.Length);
+            Assert.IsTrue(hub0KnownHubs.Any(x => x.Name == "hub 1"));
+
+            var hub1KnownHubs = hub1.KnownHubs.ToArray();
+            Assert.AreEqual(1, hub1KnownHubs.Length);
+            Assert.IsTrue(hub1KnownHubs.Any(x => x.Name == "hub 0"));
+
+            var connections0 = hub0.Connections.ToArray();
+            Assert.AreEqual(2, connections0.Length);
+            Assert.IsTrue(connections0.All(x => x.State == HubConnectionState.Active));
+
+            var connections1 = hub1.Connections.ToArray();
+            Assert.AreEqual(2, connections1.Length);
+            Assert.IsTrue(connections1.All(x => x.State == HubConnectionState.Active));
+        }
+
+        [TestMethod]
+        public void TwoConnectionsAndYetAnthorHub()
+        {
+            using var hub0 = new Hub("hub 0");
+            var hub0EndPoint = new IPEndPoint(IPAddress.Loopback, 4500);
+            hub0.StartListening(hub0EndPoint);
+
+            using var hub1 = new Hub("hub 1");
+
+            Task.WaitAll(hub1.Connect(hub0EndPoint),
+                         hub1.Connect(hub0EndPoint));
+
+            using var hub2 = new Hub("hub 2");
+            hub2.Connect(hub0EndPoint).Wait();
+
+            var hub0KnownHubs = hub0.KnownHubs.ToArray();
+            Assert.AreEqual(2, hub0KnownHubs.Length);
+            Assert.IsTrue(hub0KnownHubs.Any(x => x.Name == "hub 1"));
+            Assert.IsTrue(hub0KnownHubs.Any(x => x.Name == "hub 2"));
+
+            var hub1KnownHubs = hub1.KnownHubs.ToArray();
+            Assert.AreEqual(2, hub1KnownHubs.Length);
+            Assert.IsTrue(hub1KnownHubs.Any(x => x.Name == "hub 0"));
+            Assert.IsTrue(hub1KnownHubs.Any(x => x.Name == "hub 2"));
+
+            var hub2KnownHubs = hub2.KnownHubs.ToArray();
+            Assert.AreEqual(2, hub2KnownHubs.Length);
+            Assert.IsTrue(hub2KnownHubs.Any(x => x.Name == "hub 0"));
+            Assert.IsTrue(hub2KnownHubs.Any(x => x.Name == "hub 1"));
+
+            var connections0 = hub0.Connections.ToArray();
+            Assert.AreEqual(3, connections0.Length);
+            Assert.IsTrue(connections0.All(x => x.State == HubConnectionState.Active));
+
+            var connections1 = hub1.Connections.ToArray();
+            Assert.AreEqual(2, connections1.Length);
+            Assert.IsTrue(connections1.All(x => x.State == HubConnectionState.Active));
+
+            var connections2 = hub2.Connections.ToArray();
+            Assert.AreEqual(1, connections2.Length);
+            Assert.IsTrue(connections2.All(x => x.State == HubConnectionState.Active));
+        }
+
+        [TestMethod]
+        public void TwoConnectionsAndYetAnthorHubThenOneDisconnect()
+        {
+            using var hub0 = new Hub("hub 0");
+            var hub0EndPoint = new IPEndPoint(IPAddress.Loopback, 4500);
+            hub0.StartListening(hub0EndPoint);
+
+            using var hub1 = new Hub("hub 1");
+
+            Task.WaitAll(hub1.Connect(hub0EndPoint),
+                         hub1.Connect(hub0EndPoint));
+
+            using var hub2 = new Hub("hub 2");
+            hub2.Connect(hub0EndPoint).Wait();
+
+            hub1.Connections.First().Disconnect();
+
+            Thread.Sleep(10);
+
+            var hub0KnownHubs = hub0.KnownHubs.ToArray();
+            Assert.AreEqual(2, hub0KnownHubs.Length);
+            Assert.IsTrue(hub0KnownHubs.Any(x => x.Name == "hub 1"));
+            Assert.IsTrue(hub0KnownHubs.Any(x => x.Name == "hub 2"));
+
+            var hub1KnownHubs = hub1.KnownHubs.ToArray();
+            Assert.AreEqual(2, hub1KnownHubs.Length);
+            Assert.IsTrue(hub1KnownHubs.Any(x => x.Name == "hub 0"));
+            Assert.IsTrue(hub1KnownHubs.Any(x => x.Name == "hub 2"));
+
+            var hub2KnownHubs = hub2.KnownHubs.ToArray();
+            Assert.AreEqual(2, hub2KnownHubs.Length);
+            Assert.IsTrue(hub2KnownHubs.Any(x => x.Name == "hub 0"));
+            Assert.IsTrue(hub2KnownHubs.Any(x => x.Name == "hub 1"));
+
+            var connections0 = hub0.Connections.ToArray();
+            Assert.AreEqual(2, connections0.Length);
+            Assert.IsTrue(connections0.All(x => x.State == HubConnectionState.Active));
+
+            var connections1 = hub1.Connections.ToArray();
+            Assert.AreEqual(1, connections1.Length);
+            Assert.IsTrue(connections1.All(x => x.State == HubConnectionState.Active));
+
+            var connections2 = hub2.Connections.ToArray();
+            Assert.AreEqual(1, connections2.Length);
+            Assert.IsTrue(connections2.All(x => x.State == HubConnectionState.Active));
+        }
+    }
+}
---
+++ UnitTests/RemoteCalling.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NiL.Hub;
+
+namespace Tests.Hubs
+{
+    [TestClass]
+    public sealed class RemoteCalling
+    {
+        private interface ITestInterface
+        {
+            int sum(int a, int b);
+        }
+
+        private sealed class TestImplementation : ITestInterface
+        {
+            public int sum(int a, int b)
+            {
+                return a + b;
+            }
+        }
+
+        [TestMethod]
+        [Timeout(1000)]
+        public void InterfacePropagation()
+        {
+            using var hub1 = new Hub(777001, "hub 1");
+            using var hub2 = new Hub(777002, "hub 2");
+            var endpoint = new IPEndPoint(IPAddress.Loopback, 4500);
+            hub1.StartListening(endpoint);
+            hub2.Connect(endpoint).Wait();
+
+            hub1.RegisterInterface<ITestInterface>(new TestImplementation()).Wait();
+
+            while (!hub2.TryGet<ITestInterface>(out _))
+                Thread.Sleep(1);
+
+            var remoteInterface = hub2.Get<ITestInterface>();
+
+            Assert.IsNotNull(remoteInterface);
+            Assert.AreEqual(typeof(ITestInterface).FullName, remoteInterface.Name);
+        }
+
+        [TestMethod]
+        [Timeout(1000)]
+        public void RemoteCall()
+        {
+            using var hub1 = new Hub(777003, "hub 1");
+            using var hub2 = new Hub(777004, "hub 2");
+            var endpoint = new IPEndPoint(IPAddress.Loopback, 4500);
+            hub1.StartListening(endpoint);
+            hub2.Connect(endpoint).Wait();
+
+            hub1.RegisterInterface<ITestInterface>(new TestImplementation()).Wait();
+
+            while (!hub2.TryGet<ITestInterface>(out _))
+                Thread.Sleep(1);
+
+            var a = 1;
+            var b = 2;
+            var remoteInterface = hub2.Get<ITestInterface>();
+            var result = remoteInterface.Call(_ => _.sum(a, b));
+
+            GC.Collect(2);
+
+            result.Wait();
+
+            Assert.AreEqual(3, result.Result);
+        }
+
+        [TestMethod]
+        [Timeout(1000)]
+        public void RemoteCallWithMediator()
+        {
+            using var hub1 = new Hub(777005, "hub 1");
+            var endpoint1 = new IPEndPoint(IPAddress.Loopback, 4500);
+
+            using var hub2 = new Hub(777006, "hub 2");
+            var endpoint2 = new IPEndPoint(IPAddress.Loopback, 4501);
+
+            using var hub3 = new Hub(777007, "hub 3");
+
+            hub1.StartListening(endpoint1);
+            hub2.Connect(endpoint1).Wait();
+
+            hub2.StartListening(endpoint2);
+            hub3.Connect(endpoint2).Wait();
+
+            hub1.RegisterInterface<ITestInterface>(new TestImplementation()).Wait();
+
+            while (!hub3.TryGet<ITestInterface>(out _))
+                Thread.Sleep(1);
+
+            var a = 1;
+            var b = 2;
+            var remoteInterface = hub3.Get<ITestInterface>();
+            var result = remoteInterface.Call(_ => _.sum(a, b));
+
+            GC.Collect(2);
+
+            result.Wait();
+
+            Assert.AreEqual(3, result.Result);
+        }
+    }
+}
